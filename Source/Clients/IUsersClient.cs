0439d44fa98cede35ede27452326027fb9f4ed65
Richard Garcia <code.richardy@gmail.com>
2025-09-16 18:37:48 -0300

    this commit introduces the iusersclient interface for user management, permissions, and group assignments in the sdk

---
+++ Source/Clients/IUsersClient.cs
@@ -0,0 +1,48 @@
+namespace Vinder.IdentityProvider.Sdk.Contracts.Clients;
+
+public interface IUsersClient
+{
+    public Task<Result<Pagination<UserDetails>>> GetUsersAsync(
+        UsersFetchParameters parameters,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result<IReadOnlyCollection<PermissionDetails>>> GetUserPermissionsAsync(
+        Guid userId,
+        ListUserAssignedPermissionsParameters? parameters = null,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result<IReadOnlyCollection<GroupBasicDetails>>> GetUserGroupsAsync(
+        Guid userId,
+        ListUserAssignedGroupsParameters? parameters = null,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> DeleteUserAsync(
+        Guid userId,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> AssignUserToGroupAsync(
+        AssignUserToGroupContext data,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> AssignUserPermissionAsync(
+        AssignUserPermissionContext data,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> RevokeUserPermissionAsync(
+        Guid userId,
+        Guid permissionId,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> RemoveUserFromGroupAsync(
+        Guid userId,
+        Guid groupId,
+        CancellationToken cancellation = default
+    );
+}
