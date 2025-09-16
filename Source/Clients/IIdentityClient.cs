bca788475b674f4262fa08e082bacf9673d3e870
Richard Garcia <code.richardy@gmail.com>
2025-09-16 18:37:40 -0300

    this commit introduces the iidentityclient interface for authentication and identity management operations in the sdk

---
+++ Source/Clients/IIdentityClient.cs
@@ -0,0 +1,19 @@
+namespace Vinder.IdentityProvider.Sdk.Contracts.Clients;
+
+public interface IIdentityClient
+{
+    public Task<Result<AuthenticationResult>> AuthenticateAsync(
+        AuthenticationCredentials credentials,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> CreateIdentityAsync(
+        IdentityEnrollmentCredentials credentials,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result> InvalidateSessionAsync(
+        SessionInvalidation session,
+        CancellationToken cancellation = default
+    );
+}
