079bdba71d9f93b476efd2efc8f1ae0b01bdc027
Richard Garcia <code.richardy@gmail.com>
2025-09-16 18:53:53 -0300

    this commit introduces the usings file to globally import namespaces for internal types and all payloads, enabling simplified references throughout the sdk contracts and improving code maintainability

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -5,7 +5,7 @@ global using Vinder.IdentityProvider.Sdk.Contracts.Payloads.User;
 
 global using Vinder.IdentityProvider.Sdk.Contracts.Payloads.Permission;
 global using Vinder.IdentityProvider.Sdk.Contracts.Payloads.Tenant;
-
 global using Vinder.IdentityProvider.Sdk.Contracts.Payloads.Group;
+
 global using Vinder.IdentityProvider.Sdk.Contracts.Payloads.Common;
 global using Vinder.IdentityProvider.Sdk.Contracts.Payloads.Identity;
